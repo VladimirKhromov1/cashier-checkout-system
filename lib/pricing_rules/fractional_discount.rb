53843d024887a0e5395e9e47ea0934814caac2a6
vladimir <v.khromov@datarockets.com>
2025-08-20 20:19:42 +0300

    Refactor FractionalDiscount to use TypeValidator and freeze protection

--- lib/pricing_rules/fractional_discount.rb
+++ lib/pricing_rules/fractional_discount.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 require_relative 'base'
+require_relative '../support/type_validator'
 
 module PricingRule
   class FractionalDiscount < Base
     def initialize(product_code, min_quantity:, numerator:, denominator:)
       super(product_code)
-      @min_quantity = min_quantity
-      @ratio = Rational(numerator, denominator)
+      @min_quantity = TypeValidator.validate_positive_integer!(min_quantity, 'Minimum quantity')
+      @ratio = validate_ratio!(numerator, denominator)
+      freeze
     end
 
     def total_price_in_pence(product, quantity)
@@ -18,5 +20,16 @@ module PricingRule
     private
 
     attr_reader :min_quantity, :ratio
+
+    def validate_ratio!(numerator, denominator)
+      num = TypeValidator.validate_positive_integer!(numerator, 'Numerator')
+      den = TypeValidator.validate_positive_integer!(denominator, 'Denominator')
+
+      unless num < den
+        raise ArgumentError, "Numerator (#{num}) must be less than denominator (#{den}) for a discount rule"
+      end
+
+      Rational(num, den)
+    end
   end
 end
