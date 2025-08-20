ae6c1189993231acfc79475b6055f8044deaf34f
vladimir <v.khromov@datarockets.com>
2025-08-20 18:25:09 +0300

    Refactor Product class with validations for input fields and protect against changing

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,25 @@
+require_relative 'support/type_validator'
+
 class Product
+  SUPPORTED_CURRENCIES = ['GBP'].freeze
+
   attr_reader :code, :name, :price_in_pence, :currency
 
   def initialize(code:, name:, price_in_pence:, currency:)
-    @code = code
-    @name = name
-    @price_in_pence = price_in_pence
-    @currency = currency
+    @code = TypeValidator.validate_string!(code, 'Product code')
+    @name = TypeValidator.validate_string!(name, 'Product name')
+    @price_in_pence = TypeValidator.validate_positive_integer!(price_in_pence, 'Price in pence')
+    @currency = validate_currency!(currency)
+    freeze
+  end
+
+  private
+
+  def validate_currency!(currency)
+    curr = TypeValidator.validate_string!(currency, 'Currency').upcase
+    unless SUPPORTED_CURRENCIES.include?(curr)
+      raise ArgumentError, "Unsupported currency: '#{curr}'. Supported: #{SUPPORTED_CURRENCIES.join(', ')}"
+    end
+    curr
   end
 end
