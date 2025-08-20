e696c6a792443bc8299487a090a4a868d926f6a9
vladimir <v.khromov@datarockets.com>
2025-08-20 20:25:44 +0300

    Refactor BuyOneGetOneFree to use freeze protection

--- lib/pricing_rules/buy_one_get_one_free.rb
+++ lib/pricing_rules/buy_one_get_one_free.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
-require_relative '../../lib/pricing_rules/base'
+require_relative 'base'
 
 module PricingRule
   class BuyOneGetOneFree < Base
+    def initialize(product_code)
+      super(product_code)
+      freeze
+    end
+
     def total_price_in_pence(product, quantity)
       return 0 if quantity <= 0
 
